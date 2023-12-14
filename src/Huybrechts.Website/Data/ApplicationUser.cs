d869a2fa5a53cb1b5f96eaed50778aabfbd21a65
Vincent Huybrechts <vhuybrechts@omp.com>
2023-12-14 13:42:40 +0100

    Updated privacy policy and linked to linkedin

--- src/Huybrechts.Website/Data/ApplicationUser.cs
+++ src/Huybrechts.Website/Data/ApplicationUser.cs
@@ -5,5 +5,28 @@ namespace Huybrechts.Website.Data
 	// Add profile data for application users by adding properties to the ApplicationUser class
 	public class ApplicationUser : IdentityUser
 	{
+		public static bool IsAdministrator(System.Security.Claims.ClaimsPrincipal user)
+		{
+			return user.IsInRole(ApplicationRole.Administrator);
+		}
+
+		public static bool IsUser(System.Security.Claims.ClaimsPrincipal user)
+		{
+			return user.IsInRole(ApplicationRole.User);
+		}
+
+		public string? FirstName { get; set; }
+
+		public string? LastName { get; set; }
+
+		public string Fullname => FirstName + (FirstName?.Length > 0 && LastName?.Length > 0 ? " " : "") + LastName;
+
+		public string? ImageType { get; set; }
+
+		public string? ImageData { get; set; }
+
+		public string? ImageData32 { get; set; }
+
+		public string? ImageData64 { get; set; }
 	}
 }
