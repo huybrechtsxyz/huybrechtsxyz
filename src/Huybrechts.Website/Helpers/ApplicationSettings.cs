b9b72a4d8684e61c832b10dd522f73ace1d3081e
Vincent Huybrechts <vhuybrechts@omp.com>
2023-12-21 17:12:54 +0100

    Creation of index and privacy

---
+++ src/Huybrechts.Website/Helpers/ApplicationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Huybrechts.Website.Helpers;
+
+public class ApplicationSettings
+{
+	private readonly IConfiguration _configuration;
+
+	public static CultureInfo[] GetSupportedCultures() => [new CultureInfo("EN"), new CultureInfo("NL")];
+
+	public static CultureInfo GetDefaultCulture() => new CultureInfo("EN");
+
+	public ApplicationSettings(IConfiguration configuration)
+	{
+		_configuration = configuration;
+	}
+
+}
--- src/Huybrechts.Website/Program.cs
+++ src/Huybrechts.Website/Program.cs
@@ -1,8 +1,12 @@
 using Huybrechts.Website.Components;
+using Huybrechts.Website.Helpers;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.ResponseCompression;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
+using System.Globalization;
 using System.IO.Compression;
 
 try 
@@ -24,8 +28,20 @@ try
         writeToProviders: true);
 
     Log.Information("Add services to the container");
+    builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+    builder.Services.AddControllers();
+    builder.Services.AddRazorPages()
+        .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
     builder.Services.AddRazorComponents()
         .AddInteractiveServerComponents();
+
+    Log.Information("Add cookie policy options");
+    builder.Services.Configure<CookiePolicyOptions>(options =>
+    {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+    });
 
     Log.Information("Enabling response compression with brotli and gzip");
     builder.Services.AddResponseCompression(options => {
@@ -73,15 +89,26 @@ try
     app.UseResponseCompression();
     app.UseHttpsRedirection();
     app.UseStaticFiles();
+    app.UseCookiePolicy();
     app.UseAntiforgery();
 
+    Log.Information("Initializing application localization");
+    app.UseRequestLocalization(new RequestLocalizationOptions
+    {
+        SupportedCultures = ApplicationSettings.GetSupportedCultures(),
+        SupportedUICultures = ApplicationSettings.GetSupportedCultures(),
+        DefaultRequestCulture = new RequestCulture(ApplicationSettings.GetDefaultCulture())
+    });
+
     Log.Information("Mapping and routing razor components");
+    app.UseSerilogRequestLogging();
+    app.MapControllers();
+    app.MapRazorPages();
     app.MapRazorComponents<App>()
         .AddInteractiveServerRenderMode();
 
     Log.Information("Run configured application");
     app.Run();
-
 }
 catch (Exception ex)
 {
