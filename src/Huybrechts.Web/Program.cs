4054a28cfc6b3db4678acfd53fc0211dbc40e103
Vincent Huybrechts <vhuybrechts@omp.com>
2024-01-05 00:20:24 +0100

    Added config files and serilog

--- src/Huybrechts.Web/Program.cs
+++ src/Huybrechts.Web/Program.cs
@@ -1,6 +1,43 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
+using Serilog.Formatting.Compact;
 
-app.MapGet("/", () => "Hello World!");
+try
+{
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateBootstrapLogger();
+    Log.Information("Starting application");
 
-app.Run();
+    Log.Information("Creating application builder");
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(new RenderedCompactJsonFormatter()),
+        writeToProviders: true);
+    
+    Log.Information("Building the application and services");
+    foreach (var service in builder.Services)
+        Log.Information(service.ToString());
+
+    Log.Information("Building the application and services");
+    var app = builder.Build();
+
+    Log.Information("Mapping and routing razor components");
+    app.MapGet("/", () => "Hello World!");
+
+    Log.Information("Run configured application");
+    app.Run();
+}
+catch(Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception in application");
+}
+finally
+{
+    Log.Information("Application shut down complete");
+    Log.CloseAndFlush();
+}
