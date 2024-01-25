cb8df433075ac45c17830301e2faa8194f429912
Vincent Huybrechts <vhuybrechts@omp.com>
2024-01-25 09:03:12 +0100

    Remove consul

--- src/website/Huybrechts.Infra/Extensions/ConfigurationBuilderExtensions.cs
+++ src/website/Huybrechts.Infra/Extensions/ConfigurationBuilderExtensions.cs
@@ -1,8 +1,5 @@
-﻿using Consul;
-using Huybrechts.Infra.Config;
+﻿using Huybrechts.Infra.Config;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Winton.Extensions.Configuration.Consul;
 
 namespace Huybrechts.Infra.Extensions;
 
@@ -18,52 +15,5 @@ public static class ConfigurationBuilderExtensions
 			secretsDirectoryPath,
 			colonPlaceholder,
 			allowedPrefixes));
-	}
-
-	/// <summary>
-	/// public ValuesController(IConsulClient consulClient)  
-	/// var res = await _consulClient.KV.Get(key);
-	/// 
-	/// public class DemoAppOptions
-	/// services.Configure<DemoAppOptions>(Configuration.GetSection("DemoAppOptions"));
-	/// public ValuesController(IOptions<DemoAppSettings> options)
-	/// </summary>
-	/// <param name="services"></param>
-	/// <param name="configuration"></param>
-	/// <returns></returns>
-	public static IServiceCollection AddConsulConfig(this IServiceCollection services, IConfiguration configuration)
-	{
-		var address = configuration.GetConnectionString("ConsulHost");
-		if (string.IsNullOrEmpty(address))
-			return services;
-
-		
-
-		services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig => { consulConfig.Address = new Uri(address); }
-		, null, handlerOverride =>
-		{
-			//disable proxy of httpclienthandler  
-			handlerOverride.Proxy = null;
-			handlerOverride.UseProxy = false;
-		}));
-		return services;
-	}
-
-	public static IConfigurationBuilder AddConsulBuilder(this IConfigurationBuilder builder, IConfiguration configuration)
-	{
-		var address = configuration.GetConnectionString("ConsulHost");
-		if (string.IsNullOrEmpty(address))
-			return builder;
-
-		builder.AddConsul("Website");
-
-		services.AddSingleton<IConsulClient, ConsulClient>(p => new ConsulClient(consulConfig => { consulConfig.Address = new Uri(address); }
-		, null, handlerOverride =>
-		{
-			//disable proxy of httpclienthandler  
-			handlerOverride.Proxy = null;
-			handlerOverride.UseProxy = false;
-		}));
-		return services;
 	}
 }
