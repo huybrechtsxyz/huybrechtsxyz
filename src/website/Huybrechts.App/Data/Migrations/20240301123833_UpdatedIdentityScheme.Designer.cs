108397614c0f48c81ad183624e90455c64697eb8
Vincent Huybrechts <vhuybrechts@omp.com>
2024-03-01 13:39:09 +0100

    Added migration of update to identity entities

--- src/website/Huybrechts.App/Data/Migrations/20240301123833_UpdatedIdentityScheme.Designer.cs
+++ src/website/Huybrechts.App/Data/Migrations/20240301123833_UpdatedIdentityScheme.Designer.cs
@@ -4,6 +4,7 @@ using Huybrechts.App.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Huybrechts.App.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240301123833_UpdatedIdentityScheme")]
+    partial class UpdatedIdentityScheme
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
