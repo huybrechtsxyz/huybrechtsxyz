9b3a1d6c95711c149e4520da81eff7c0d7780ccc
Vincent Huybrechts <vhuybrechts@omp.com>
2024-05-17 13:23:01 +0200

    Weird usings

--- src/website/Huybrechts.App/Identity/Entities/ApplicationUserLogin.cs
+++ src/website/Huybrechts.App/Identity/Entities/ApplicationUserLogin.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Mono.TextTemplating;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Huybrechts.App.Identity.Entities;
