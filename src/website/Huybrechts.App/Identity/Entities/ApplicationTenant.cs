7cf78e78770b8c42efb5f2116e791f44b9e86c48
Vincent Huybrechts <vhuybrechts@omp.com>
2024-05-22 12:01:31 +0200

    Moving applicationtenant to core project

--- src/website/Huybrechts.App/Identity/Entities/ApplicationTenant.cs
+++ src/website/Huybrechts.App/Identity/Entities/ApplicationTenant.cs
@@ -1,39 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Huybrechts.Core.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Huybrechts.App.Identity.Entities;
 
 [Table("ApplicationTenant")]
-public sealed record ApplicationTenant
+public sealed record ApplicationTenant : ApplicationTenantInfo
 {
-    [Key]
-    [Required]
-    [RegularExpression("^[a-z0-9]+$")]
-    [StringLength(24, MinimumLength = 2)]
-    public string Id { get; set; } = string.Empty;
-
-    public ApplicationTenantState State { get; set; }
-
-    [Required]
-    [StringLength(256, MinimumLength = 2)]
-    public string Name { get; set; } = string.Empty;
-
-    [StringLength(512)]
-    public string? Description { get; set; }
-
-    public string? Remark { get; set; }
-
-    public byte[]? Picture { get; set; }
-
-    [StringLength(32)]
-	public string? DatabaseProvider { get; set; } = string.Empty;
-
-	[StringLength(512)]
-    public string? ConnectionString { get; set; }
-
-    [Timestamp]
-    public byte[]? ConcurrencyStamp { get; set; }
-
     public void UpdateFrom(ApplicationTenant entity)
     {
         this.Name = entity.Name;
