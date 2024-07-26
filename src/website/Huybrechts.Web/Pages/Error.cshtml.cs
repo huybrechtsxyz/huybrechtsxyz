9aaee1f40f14da8baebc324c75d79e8e78eafb9c
Vincent Huybrechts <vhuybrechts@omp.com>
2024-07-26 13:40:43 +0200

    Updated to nifty layout

--- src/website/Huybrechts.Web/Pages/Error.cshtml.cs
+++ src/website/Huybrechts.Web/Pages/Error.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
@@ -6,13 +5,26 @@ namespace Huybrechts.Web.Pages
 {
     public class ErrorModel : PageModel
     {
-        public string? ErrorCode { get; set; }
+        public string ErrorCode { get; set; }
 
-        public string? ErrorText { get; set; }
+        public string ErrorText { get; set; }
+
+        public string ErrorStyle => ErrorCode[..1] switch
+        {
+            "4" => "bg-warning",
+            _ => "bg-danger"
+        };
 
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public ErrorModel()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            ErrorCode = "400";
+            ErrorText = Microsoft.AspNetCore.WebUtilities.ReasonPhrases.GetReasonPhrase(400);
+        }
 
         public void OnGet(int status)
         {
