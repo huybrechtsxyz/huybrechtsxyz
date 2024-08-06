68d3e188b7814493bbe9068397cab73b764b0e6c
Vincent Huybrechts <vhuybrechts@omp.com>
2024-08-06 09:10:44 +0200

    Dynamic queries

---
+++ src/website/Huybrechts.App/Features/EntityListFlow.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Huybrechts.App.Data;
+using Huybrechts.Core;
+using MediatR;
+using System.Linq.Dynamic.Core;
+
+namespace Huybrechts.App.Features;
+
+public static class EntityListFlow
+{
+    public class Query
+    {
+        public string CurrentFilter { get; init; } = string.Empty;
+
+        public string SearchText { get; init; } = string.Empty;
+
+        public string SortOrder { get; init; } = string.Empty;
+
+        public int? Page { get; init; }
+    }
+
+    public class Result<TModel>
+    {
+        public string CurrentFilter { get; init; } = string.Empty;
+
+        public string SearchText { get; init; } = string.Empty;
+
+        public string SortOrder { get; init; } = string.Empty;
+
+        public PaginatedList<TModel> Results { get; init; } = [];
+    }
+
+    public class Handler<TEntity, TModel> where TEntity : Entity
+    {
+        private readonly FeatureContext _dbcontext;
+        private readonly IConfigurationProvider _configuration;
+        
+        internal IQueryable<TEntity> EntitySet => _dbcontext.Set<TEntity>();
+
+        internal string? SetWhere { get; set; }
+
+        internal string? OrderBy { get; set; }
+
+        public Handler(FeatureContext dbcontext, IConfigurationProvider configuration)
+        {
+            _dbcontext = dbcontext;
+            _configuration = configuration;
+        }
+
+        public async Task<Result<TModel>> Handle(Query request, CancellationToken token)
+        {
+            IQueryable<TEntity> query = EntitySet;
+
+            var searchString = request.SearchText ?? request.CurrentFilter;
+            if (!string.IsNullOrEmpty(SetWhere) && !string.IsNullOrEmpty(searchString))
+            {
+                query = query.Where(SetWhere, searchString);
+            }
+
+            if (OrderBy is not null)
+            {
+                query.OrderBy(OrderBy);
+            }
+            
+            int pageSize = 50;
+            int pageNumber = request.Page ?? 1;
+            var results = await query
+                .ProjectTo<TModel>(_configuration)
+                .PaginatedListAsync(pageNumber, pageSize);
+
+            var model = new Result<TModel>
+            {
+                CurrentFilter = searchString,
+                SearchText = searchString,
+                SortOrder = request.SortOrder,
+                Results = results ?? []
+            };
+
+            return model;
+        }
+    }
+}
---
+++ src/website/Huybrechts.App/Features/Platform/PlatformInfoFlow.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Huybrechts.Core.Platform;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Huybrechts.App.Features.Platform;
+
+public static class PlatformInfoFlow
+{
+    public record Model
+    {
+        public Ulid Id { get; init; }
+
+        [Display(Name = nameof(Name), ResourceType = typeof(PlatformLocalization))]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = nameof(Description), ResourceType = typeof(PlatformLocalization))]
+        public string? Description { get; set; }
+
+        [Display(Name = nameof(Remark), ResourceType = typeof(PlatformLocalization))]
+        public string? Remark { get; set; }
+    }
+
+    //
+    // LIST
+    //
+
+    public sealed record ListModel : Model
+    {
+    }
+
+    public sealed class ListMapping : Profile
+    {
+        public ListMapping() => CreateProjection<PlatformInfo, ListModel>();
+    }
+
+    public sealed class ListQuery : EntityListFlow.Query, IRequest<ListResult> 
+    {
+    }
+
+    public sealed class ListValidator : AbstractValidator<ListQuery> { public ListValidator() { } }
+
+    public sealed class ListResult : EntityListFlow.Result<ListModel>
+    {
+    }
+
+    public sealed class ListHandler : 
+        EntityListFlow.Handler<PlatformInfo, ListModel>,
+        IRequestHandler<ListQuery, ListResult>
+    {
+        public ListHandler(PlatformContext dbcontext, IConfigurationProvider configuration)
+            : base(dbcontext, configuration) 
+        {
+            base.SetWhere = "Name.Contains(@0) || Description.Contains(@0)";
+            base.OrderBy = "Name";
+        }
+
+        public async Task<ListResult> Handle(ListQuery request, CancellationToken cancellationToken)
+        {
+            return (ListResult)await base.Handle(request, cancellationToken);
+        }
+    }
+}
