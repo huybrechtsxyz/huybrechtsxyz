c1994ef0de2da2bf9e1d7a35bcfeeb5817a24c02
Vincent Huybrechts <vhuybrechts@omp.com>
2024-08-08 08:59:49 +0200

    Application localization

--- src/website/Huybrechts.Core/Application/ApplicationRoleDefaults.cs
+++ src/website/Huybrechts.Core/Application/ApplicationRoleDefaults.cs
@@ -4,30 +4,30 @@ namespace Huybrechts.Core.Application;
 
 public enum ApplicationSystemRole
 {
-    [Display(Name = nameof(Administrator), Description = nameof(Administrator) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(Administrator), Description = nameof(Administrator) + "_d", ResourceType = typeof(Localization))]
     Administrator,
 
-    [Display(Name = nameof(User), Description = nameof(User) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(User), Description = nameof(User) + "_d", ResourceType = typeof(Localization))]
     User
 }
 
 public enum ApplicationTenantRole
 {
-    [Display(Name = nameof(None), Description = nameof(None) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(None), Description = nameof(None) + "_d", ResourceType = typeof(Localization))]
     None,
 
-    [Display(Name = nameof(Owner), Description = nameof(Owner) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(Owner), Description = nameof(Owner) + "_d", ResourceType = typeof(Localization))]
     Owner,
 
-    [Display(Name = nameof(Manager), Description = nameof(Manager) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(Manager), Description = nameof(Manager) + "_d", ResourceType = typeof(Localization))]
     Manager,
 
-    [Display(Name = nameof(Contributor), Description = nameof(Contributor) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(Contributor), Description = nameof(Contributor) + "_d", ResourceType = typeof(Localization))]
     Contributor,
 
-    [Display(Name = nameof(Member), Description = nameof(Member) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(Member), Description = nameof(Member) + "_d", ResourceType = typeof(Localization))]
     Member,
 
-    [Display(Name = nameof(Guest), Description = nameof(Guest) + "_d", ResourceType = typeof(ApplicationLocalization))]
+    [Display(Name = nameof(Guest), Description = nameof(Guest) + "_d", ResourceType = typeof(Localization))]
     Guest
 }
--- src/website/Huybrechts.Core/Application/Localization.Designer.cs
+++ src/website/Huybrechts.Core/Application/Localization.Designer.cs
@@ -22,14 +22,14 @@ namespace Huybrechts.Core.Application {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class ApplicationLocalization {
+    public class Localization {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ApplicationLocalization() {
+        internal Localization() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Huybrechts.Core.Application {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Huybrechts.Core.Application.ApplicationLocalization", typeof(ApplicationLocalization).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Huybrechts.Core.Application.Localization", typeof(Localization).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
