933597ab707558ecae8c5dab4e7b2454c8b2c187
Vincent Huybrechts <vhuybrechts@omp.com>
2024-08-12 23:12:13 +0200

    Localization per feature for app

--- src/website/Huybrechts.App/Features/Platform/Localization.Designer.cs
+++ src/website/Huybrechts.App/Features/Platform/Localization.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Huybrechts.App.Features {
+namespace Huybrechts.App.Features.Platform {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Huybrechts.App.Features {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Huybrechts.App.Features.Localization", typeof(Localization).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Huybrechts.App.Features.Platform.Localization", typeof(Localization).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
