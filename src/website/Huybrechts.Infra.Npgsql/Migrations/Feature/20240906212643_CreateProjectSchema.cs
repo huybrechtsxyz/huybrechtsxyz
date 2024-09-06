b081612cbf59f171aa72208682f6345b8bc84dc6
Vincent Huybrechts <vhuybrechts@omp.com>
2024-09-06 23:27:39 +0200

    Add proejct and project design

---
+++ src/website/Huybrechts.Infra.Npgsql/Migrations/Feature/20240906212643_CreateProjectSchema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Huybrechts.Infra.Npgsql.Migrations.Feature
+{
+    /// <inheritdoc />
+    public partial class CreateProjectSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
