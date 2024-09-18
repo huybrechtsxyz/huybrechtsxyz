9e94c60fdbdcf2b275f526428e4c49656e4f853a
Vincent Huybrechts <vhuybrechts@omp.com>
2024-09-18 21:48:16 +0200

    Update to project plantuml

--- src/website/Huybrechts.Web/wwwroot/js/layout.js
+++ src/website/Huybrechts.Web/wwwroot/js/layout.js
@@ -2,6 +2,6 @@
     document.getElementById('headerThemeToggler').addEventListener('change', (e) => {
         var value = e.target.checked ? 'on' : 'off';
         $.post("/Application/SetTheme?theme=" + value);
-        console.log(value)
+        //console.log(value)
     })
 })
