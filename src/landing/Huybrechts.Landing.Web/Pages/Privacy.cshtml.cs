d2f233c8c9379830a2e19ea2bcc75f3252e1daff
Vincent Huybrechts <vhuybrechts@omp.com>
2024-10-30 23:35:13 +0100

    Updating index page

--- src/landing/Huybrechts.Landing.Web/Pages/Privacy.cshtml.cs
+++ src/landing/Huybrechts.Landing.Web/Pages/Privacy.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Huybrechts.Landing.Web.Pages
 {
+    [AllowAnonymous]
     public class PrivacyModel : PageModel
     {
         public void OnGet()
---
+++ src/landing/Huybrechts.Landing.Web/wwwroot/js/index.js
@@ -0,0 +1,200 @@
+$(document).ready(function () {
+    'use strict';
+
+    //********* page loader js
+
+    //setTimeout(function () {
+    //    $('.loader_bg').fadeToggle();
+    //}, 1500);
+
+	//********** menu background color change while scroll
+
+	$(window).on('scroll', function () {
+		var menu_area = $('.nav-area');
+		if ($(window).scrollTop() > 200) {
+			menu_area.addClass('sticky_navigation');
+		} else {
+			menu_area.removeClass('sticky_navigation');
+		}
+	});
+	
+	//*********** Animated headline js
+
+	$('.animate-scale').animatedHeadline({
+		animationType: 'clip'
+	});
+
+	//***** Skill bar js
+
+	var skillbar = $(".skillbar");
+
+	skillbar.waypoint(function () {
+		skillbar.each(function () {
+			$(this).find(".skillbar-child").animate({
+				width: $(this).data("percent")
+			}, 1000);
+		});
+	}, {
+		offset: "80%"
+	});
+
+    //*************** Isotope filter
+
+    var $Container = $('#img-filter');
+    if( $Container.length>0 ) {
+        $Container.isotope({
+            itemSelector: '.single-port',
+            transitionDuration: '0.8s'
+        });
+        $(".img-filter").on("click", function (e){
+            $(".img-filter.active").removeClass("active");
+            $(this).addClass("active");
+            var selector = $(this).attr('data-filter');
+            $Container.isotope({
+                filter: selector
+            });
+            return false;
+        });
+
+        $(window).resize(function(){
+            setTimeout(function(){
+                $Container.isotope();
+            },1000);
+        }).trigger('resize');
+    }
+
+});
+
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+
+    {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 5,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true,
+        "config_demo": {
+            "hide_card": false,
+            "background_color": "#b61924",
+            "background_image": "",
+            "background_position": "50% 50%",
+            "background_repeat": "no-repeat",
+            "background_size": "cover"
+        }
+    }
+
+);
