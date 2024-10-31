adf52b85238758b7202bddfef58fcbc8c21a4081
Vincent Huybrechts <vhuybrechts@omp.com>
2024-10-31 21:37:25 +0100

    Add consul as configuration source

---
+++ src/shared/Huybrechts.Shared.App/Config/ConsulConfigurationSource.cs
@@ -0,0 +1,21 @@
+﻿using Consul;
+
+namespace Microsoft.Extensions.Configuration.Consul
+{
+    public class ConsulConfigurationSource : IConfigurationSource
+    {
+        public string Prefix { get; set; }
+        public QueryOptions Options { get; set; }
+
+        public ConsulConfigurationSource()
+        {
+            Prefix = string.Empty;
+            Options = QueryOptions.Default;
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new ConsulConfigurationProvider(() => new ConsulClient(), Options, Prefix);
+        }
+    }
+}
