99da485d8f5e464f83bc2610e520e77bd11078c0
Vincent Huybrechts <vhuybrechts@omp.com>
2024-10-31 17:17:57 +0100

    Adding test cases

---
+++ tests/landing/Huybrechts.Landing.Tests/Pages/IndexModelTests.cs
@@ -0,0 +1,32 @@
+﻿using Huybrechts.Landing.Web.Pages;
+using Microsoft.AspNetCore.Authorization;
+using System.Reflection;
+
+namespace Huybrechts.Landing.Tests.Pages;
+
+public class IndexModelTests
+{
+    [Fact]
+    public void OnGet_DoesNotThrowException()
+    {
+        // Arrange
+        var pageModel = new IndexModel();
+
+        // Act & Assert
+        var exception = Record.Exception(() => pageModel.OnGet());
+
+        // Verify that no exception is thrown
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void IndexModel_HasAllowAnonymousAttribute()
+    {
+        // Arrange
+        var allowAnonymousAttribute = typeof(IndexModel)
+            .GetCustomAttribute<AllowAnonymousAttribute>();
+
+        // Assert
+        Assert.NotNull(allowAnonymousAttribute);
+    }
+}
---
+++ tests/landing/Huybrechts.Landing.Tests/Pages/PrivacyModelTests.cs
@@ -0,0 +1,32 @@
+﻿using Huybrechts.Landing.Web.Pages;
+using Microsoft.AspNetCore.Authorization;
+using System.Reflection;
+
+namespace Huybrechts.Landing.Tests.Pages;
+
+public class PrivacyModelTests
+{
+    [Fact]
+    public void OnGet_DoesNotThrowException()
+    {
+        // Arrange
+        var pageModel = new PrivacyModel();
+
+        // Act & Assert
+        var exception = Record.Exception(() => pageModel.OnGet());
+
+        // Verify that no exception is thrown
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public void IndexModel_HasAllowAnonymousAttribute()
+    {
+        // Arrange
+        var allowAnonymousAttribute = typeof(PrivacyModel)
+            .GetCustomAttribute<AllowAnonymousAttribute>();
+
+        // Assert
+        Assert.NotNull(allowAnonymousAttribute);
+    }
+}
