37c7d9021d9e65eb4f2a53da474ef19ce4c813d0
Vincent Huybrechts <vincent@huybrechts.xyz>
2024-11-05 17:17:17 +0100

    Added compose and devscript

--- development.ps1
+++ development.ps1
@@ -8,3 +8,22 @@ $dataDir = "$baseDir/data"
 $logsDir = "$baseDir/logs"
 New-Item -ItemType Directory -Path $baseDir, $certDir, $dataDir, $logsDir -Force
 
+# Start Docker Compose
+$composeFile = "./src/compose.development.yml"
+Write-Host "Starting Docker Compose..."
+docker-compose -f $composeFile up -d
+
+# DEBUG AND TEST
+Start-Process -FilePath "msedge.exe" -ArgumentList `
+   "http://localhost:8500 http://localhost:9001", `
+   "--inprivate", "--start-maximized", "--new-window"
+
+Pause 'Press any key to stop debugging'
+Write-Output 'Stopping DEVELOPMENT environment...'
+
+# Stop Docker Compose
+Write-Host "Stopping Docker Compose..."
+docker-compose -f $composeFile down
+
+# SYSTEM
+Write-Output 'DEVELOPMENT environment stopped.'
