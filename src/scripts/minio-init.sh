6f9bd009a66aa9d295be8844aa907abd64941700
Vincent Huybrechts <vincent@huybrechts.xyz>
2024-12-11 22:47:51 +0100

    Minio-init now waits for minio to be healthy

--- src/scripts/minio-init.sh
+++ src/scripts/minio-init.sh
@@ -18,6 +18,27 @@ create_bucket_if_not_exists() {
   echo "Set read-write policy for $bucket_name bucket"
 }
 
+# Function to check if MinIO is available on port 9000
+check_minio_available() {
+  (echo > /dev/tcp/minio/9000) > /dev/null 2>&1
+  return $?
+}
+
+# Wait until MinIO is available, or until the timeout is reached
+echo "Waiting for MinIO to be available..."
+
+TIMEOUT=60
+COUNTER=0
+
+while ! check_minio_available; do
+  sleep 1
+  COUNTER=$((COUNTER+1))
+  if [ $COUNTER -ge $TIMEOUT ]; then
+    echo "Timeout reached. MinIO is not available after $TIMEOUT seconds."
+    exit 1
+  fi
+done
+
 # Main script execution
 echo "Setting MinIO alias..."
 export MINIO_ROOT_USER=$(cat $MINIO_ROOT_USER_FILE)
@@ -26,10 +47,12 @@ export MINIO_ROOT_PASSWORD=$(cat $MINIO_ROOT_PASSWORD_FILE)
 echo "MinIO Root User: $MINIO_ROOT_USER"
 echo "MinIO Root Password: $MINIO_ROOT_PASSWORD"
 
+# Wait for MinIO to be ready, with a timeout of 60 seconds (adjustable)
 mc alias set myminio http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 # Create and configure buckets
 create_bucket_if_not_exists backups
+
 # Uncomment the following lines if you want to create other buckets
 # create_bucket_if_not_exists thanos
 # create_bucket_if_not_exists loki
