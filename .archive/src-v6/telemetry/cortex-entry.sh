b22f30a892020fea7297b1d814e872e3a043cf9d
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-02-05 19:00:21 +0100

    Backup 6 of source directory

---
+++ .archive/src-v6/telemetry/cortex-entry.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Export secrets as environment variables
+export MINIO_ROOT_USER=$(cat /run/secrets/APP_ROOT_USERNAME)
+export MINIO_ROOT_PASSWORD=$(cat /run/secrets/APP_ROOT_PASSWORD)
+
+# Now run binary with the environment variables
+exec /bin/cortex \
+    -config.file=/config/cortex-config.yml \
+    -blocks-storage.s3.access-key-id=${MINIO_ROOT_USER} \
+    -blocks-storage.s3.secret-access-key=${MINIO_ROOT_PASSWORD}
