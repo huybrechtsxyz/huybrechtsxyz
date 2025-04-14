79c1bd3c2bc6085ab40ae65e4e8f9b3c64408e9e
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-04-14 22:42:43 +0200

    Add subnet configuration to private network resource in Terraform

--- deploy/main.tf
+++ deploy/main.tf
@@ -31,6 +31,11 @@ data "kamatera_image" "ubuntu" {
 resource "kamatera_network" "private-lan" {
   datacenter_id = data.kamatera_datacenter.frankfurt.id
   name = "lan-${var.environment}"
+  
+  subnet {
+    ip = "10.0.0.0"
+    bit = 23
+  }
 }
 
 # Provision manager 
