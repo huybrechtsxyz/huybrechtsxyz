7df5fb4012fff8730aaea7c57558c23235d3f521
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-04-18 21:29:06 +0200

    Working on logon and initialization.

--- scripts/terraform.ps1
+++ scripts/terraform.ps1
@@ -7,13 +7,14 @@ $APPPATH = "C:/Users/vince/Sources/huybrechtsxyz/"
 . "$APPPATH/scripts/psfunctions.ps1"
 
 # Load the secrets from the secrets file
-Get-EnvVarsFromFile "$APPPATH/.app/develop.env"
+Get-EnvVarsFromFile "$APPPATH/.app/test.env"
 Get-SecretsAsVars
 
 $env:TF_VAR_api_key = $env:KAMATERA_API_KEY
 $env:TF_VAR_api_secret = $env:KAMATERA_API_SECRET
 $env:TF_VAR_environment = $Env:ENVIRONMENT
 $env:TF_VAR_password = $Env:APP_ROOT_PASSWORD
+$env:TF_TOKEN_app_terraform_io = $Env:TF_API_SECRET
 
 # Copy terraform code to the app folder
 if (-not (Test-Path "$APPPATH/.app/deploy")) {
@@ -22,6 +23,16 @@ if (-not (Test-Path "$APPPATH/.app/deploy")) {
 remove-item -Path "$APPPATH/.app/deploy/*" -Recurse -Force
 Copy-Item -Path "$APPPATH/deploy/*" -Destination "$APPPATH/.app/deploy" -Recurse -Force
 Copy-Item -Path "$APPPATH/src/develop.env" -Destination "$APPPATH/.app" -Recurse -Force
+
+# Initialize Terraform (if not already initialized)
+./.app/terraform.exe `
+    -chdir="$APPPATH/.app/deploy" `
+    init
+    
+# Initialize Terraform (if not already initialized)
+./.app/terraform.exe `
+    -chdir="$APPPATH/.app/deploy" `
+    workspace select huybrechts-xyz-$Env:ENVIRONMENT
 
 # Initialize Terraform (if not already initialized)
 ./.app/terraform.exe `
