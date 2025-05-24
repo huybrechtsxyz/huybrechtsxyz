91a41c0e1b60ff3ee3beee360778f020f7f26835
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-05-24 16:17:32 +0200

    Refactor server output structure to use a map for improved clarity and access

--- deploy/output.tf
+++ deploy/output.tf
@@ -3,15 +3,17 @@
 # Name example: srv-shared-infra-2-1234
 # Name example: srv-shared-worker-3-1234
 output "serverdata" {
-  value = [
-    for key, srv in kamatera_server.server : {
-      role       = split("-", key)[0]
-      index      = split("-", key)[1]
-      label      = "${split("-", key)[0]}_${tonumber(split("-", key)[1]) + 1}"
-      name       = srv.name
-      ip         = srv.public_ips[0]
-      private_ip = srv.private_ips[0]
-      manager_ip = values(kamatera_server.server)[0].private_ips[0]
-    }
-  ]
+  value = {
+    server = [
+      for key, srv in kamatera_server.server : {
+        role       = split("-", key)[0]
+        index      = split("-", key)[1]
+        label      = "${split("-", key)[0]}_${tonumber(split("-", key)[1]) + 1}"
+        name       = srv.name
+        ip         = srv.public_ips[0]
+        private_ip = srv.private_ips[0]
+        manager_ip = values(kamatera_server.server)[0].private_ips[0]
+      }
+    ]
+  }
 }
