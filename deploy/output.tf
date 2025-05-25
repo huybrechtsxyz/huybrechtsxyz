2f401a19194f85870e95b974cbab621e0c2b2309
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-05-25 21:49:54 +0200

    Fix label calculation in serverdata output to use correct index

--- deploy/output.tf
+++ deploy/output.tf
@@ -8,7 +8,7 @@ output "serverdata" {
       for key, srv in kamatera_server.server : {
         role       = split("-", key)[0]
         index      = split("-", key)[1]
-        label      = "${split("-", key)[0]}_${tonumber(split("-", key)[1]) + 1}"
+        label      = "${split("-", key)[0]}_${tonumber(split("-", key)[1])}"
         name       = srv.name
         ip         = srv.public_ips[0]
         private_ip = srv.private_ips[0]
