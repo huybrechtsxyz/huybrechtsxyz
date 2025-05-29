0fa5c6831a39bc8fdf17f19a243584689d22bdfe
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-05-29 22:32:53 +0200

    Fix hostname condition and update node label for postgres deployment

--- src/postgres/configure.sh
+++ src/postgres/configure.sh
@@ -9,9 +9,9 @@ source $APP_PATH/functions.sh
 createpaths_from_metadata "postgres"
 
 # Update the worker-1 node with the postgres tag
-if [[ "$hostname" == *"infra-1"* ]]; then
+if [[ "$hostname" == *"manager-1"* ]]; then
   echo "[*] Updating infra-1 node with postgres tag..."
-  POSTGRES_NODE=$(docker node ls --format '{{.Hostname}}' | grep "worker-1")
+  POSTGRES_NODE=$(docker node ls --format '{{.Hostname}}' | grep "infra-1")
   docker node update --label-add postgres=true $POSTGRES_NODE
 fi
 
