e372dddd798f63c98310c02f43be9d46e27c3dd5
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-06-14 21:37:05 +0200

    Refactor terraform-apply script to use APP_PATH_TEMP for output file path

--- deploy/scripts/terraform-apply.sh
+++ deploy/scripts/terraform-apply.sh
@@ -10,6 +10,7 @@ rm -f main.template.tf
 cat main.tf
 
 echo "[*] ...Running terraform...INIT"
+mkdir -p $APP_PATH_TEMP
 terraform init
 
 echo "[*] ...Running terraform...PLAN"
@@ -19,6 +20,6 @@ echo "[*] ...Running terraform...APPLY"
 terraform apply -auto-approve -var-file="vars-${WORKSPACE}.tfvars" -input=false
 
 echo "[*] ...Reading Terraform output..."
-terraform output -json serverdata | jq -c '.' | tee /tmp/app/tf_output.json
+terraform output -json serverdata | jq -c '.' | tee $APP_PATH_TEMP/tf_output.json
 
-echo "[*] ...Terraform output saved to tf_output.json and /tmp/app/tf_output.json"
+echo "[*] ...Terraform output saved to tf_output.json and $APP_PATH_TEMP/tf_output.json"
