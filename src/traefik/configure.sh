a4270d46c3ea9b5cd1d8e2907d77b9c1a2946b5d
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-06-20 22:19:33 +0200

    Fix path variable in TRAEFIK deployment script and add debug output for configuration paths

--- src/traefik/configure.sh
+++ src/traefik/configure.sh
@@ -2,6 +2,8 @@
 set -e
 echo "[*] Deploying TRAEFIK to remote server $(hostname)..."
 
+echo PATH_CONF: $PATH_CONF
+
 echo ENVIRONMENT: $ENVIRONMENT
 export ENVIRONMENT=$ENVIRONMENT
 
@@ -9,7 +11,7 @@ echo DOMAIN_DEV: $DOMAIN_DEV
 export DOMAIN_DEV=$DOMAIN_DEV
 
 envsubst \
-  < "$TRAEFIK_PATH_CONF/traefik/config.template.yml" \
-  > "$TRAEFIK_PATH_CONF/traefik/config.yml"
+  < "$PATH_CONF/traefik/config.template.yml" \
+  > "$PATH_CONF/traefik/config.yml"
 
 echo "[*] Deploying TRAEFIK to remote server $(hostname)...DONE"
