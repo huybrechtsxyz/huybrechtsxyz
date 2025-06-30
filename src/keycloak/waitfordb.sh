51b906c51c1b6a5765de9709a673db37f9fd5406
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-06-30 19:26:17 +0200

    Refactor entrypoint scripts for Keycloak and Postgres to use array syntax for entrypoint and improve error handling for PostgreSQL readiness

--- src/keycloak/waitfordb.sh
+++ src/keycloak/waitfordb.sh
@@ -1,32 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-
-if [ -f "/run/secrets/PLATFORM_USERNAME" ]; then
-  POSTGRES_USER=$(cat /run/secrets/PLATFORM_USERNAME)
-else
-  echo "Error: PLATFORM_USERNAME secret file not found!" >&2
-  exit 1
-fi
 
 if [ -f "/run/secrets/PLATFORM_PASSWORD" ]; then
   POSTGRES_PASSWORD=$(cat /run/secrets/PLATFORM_PASSWORD)
+  export PGPASSWORD=$POSTGRES_PASSWORD
 else
   echo "Error: POSTGRES_PASSWORD secret file not found!" >&2
   exit 1
 fi
 
-# Wait for PostgreSQL to be ready
 echo "Waiting for PostgreSQL $POSTGRES_HOST to start..."
+
 TIMEOUT=60
-while ! pg_isready -h $POSTGRES_HOST -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
-  TIMEOUT=$((TIMEOUT-1))
-  if [ $TIMEOUT -le 0 ]; then
+
+while ! pg_isready -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" > /dev/null 2>&1; do
+  TIMEOUT=$(expr $TIMEOUT - 1)
+  if [ "$TIMEOUT" -le 0 ]; then
     echo "Error: PostgreSQL did not become ready in time!" >&2
     exit 1
   fi
+  echo "Postgres unavailable - sleeping 2 seconds..."
   sleep 2
 done
 
 echo 'Postgres is ready!' &&
 touch /pgready/pgready &&
-echo 'pgready file created.'"
+echo 'pgready file created.'
