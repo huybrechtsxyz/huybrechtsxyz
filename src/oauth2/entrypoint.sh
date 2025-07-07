a9ffb5adfd0199b5f6a388cb43edce69f6a6bb8e
huybrechtsxyz <vincent@huybrechts.xyz>
2025-07-07 11:46:01 +0200

    Add OAUTH2_TRAEFIK_COOKIE secret and update related configurations for OAuth2 Proxy

--- src/oauth2/entrypoint.sh
+++ src/oauth2/entrypoint.sh
@@ -9,8 +9,13 @@ else
   exit 1
 fi
 
+# Read all _FILE secret files to environment variables
+load_secret_files
+
 # Read all environment variables
-export OAUTH2_PROXY_CLIENT_SECRET_FILE=OAUTH2_PROXY_CLIENT_SECRET_FILE
+export OAUTH2_PROXY_COOKIE_SECRET=$OAUTH2_TRAEFIK_COOKIE
+
+export OAUTH2_PROXY_CLIENT_SECRET_FILE=$OAUTH2_PROXY_CLIENT_SECRET_FILE
 export OAUTH2_PROXY_OIDC_ISSUER_URL=$OAUTH2_PROXY_OIDC_ISSUER_URL
 export OAUTH2_PROXY_REDIRECT_URL=$OAUTH2_PROXY_REDIRECT_URL
 export OAUTH2_PROXY_UPSTREAMS=$OAUTH2_PROXY_UPSTREAMS
@@ -19,10 +24,6 @@ export OAUTH2_PROXY_COOKIE_NAME=$OAUTH2_PROXY_COOKIE_NAME
 export OAUTH2_PROXY_COOKIE_DOMAINS=$OAUTH2_PROXY_COOKIE_DOMAINS
 export OAUTH2_PROXY_WHITELIST_DOMAINS=$OAUTH2_PROXY_WHITELIST_DOMAINS
 
-OAUTH2_PROXY_COOKIE_SECRET=$(dd if=/dev/urandom bs=32 count=1 2>/dev/null | base64 | tr -d -- '\n' | tr -- '+/' '-_' ; echo)
-export OAUTH2_PROXY_COOKIE_SECRET
-
 exec oauth2-proxy \
   --config=/etc/config.cfg \
-  --insecure-oidc-skip-issuer-verification \
-  --allowed-roles=sysadmins
+  --insecure-oidc-skip-issuer-verification
