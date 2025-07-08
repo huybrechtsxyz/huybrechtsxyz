f05a8c7f9640fb072b12d12184f1a058df7b9888
Vincent Huybrechts <vincent@huybrechts.xyz>
2025-07-08 22:47:50 +0200

    Remove OAuth2 Proxy configuration and related scripts; update Traefik middleware to use basic authentication

--- src/oauth2/configure.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-echo "[*] Deploying OAUTH2 to remote server $(hostname)..."
-echo "[*] Deploying OAUTH2 to remote server $(hostname)...DONE"
--- src/oauth2/develop.ps1
+++
@@ -1,3 +0,0 @@
-# Configure and run service
-Write-Host "[*] ....Configuring service ... "
-Write-Host "[+] ....Configuring service ... OK"
